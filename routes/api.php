9910ba95bf6830c48e1be430118b9983922e4009
ToXxXika <mabrouki552@gmail.com>
2023-11-02 01:39:25 -0700

    Initialisation

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\EmployeeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+Route:: get('employees',[EmployeeController::class,'getEmployee']);
+Route::get('employee/{id}',[EmployeeController::class,'getEmployeeById']);
+Route::post('addEmployee',[EmployeeController::class,'addEmployee']);
+Route::put('updateEmployee/{id}',[EmployeeController::class,'updateEmployee']);
+Route::delete('deleteEmployee/{id}',[EmployeeController::class,'deleteEmployee']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
